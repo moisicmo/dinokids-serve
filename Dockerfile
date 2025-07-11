afe2bf6a7f8724f0bc332daf264cf52a2b17e325
moises <moisic.mo@gmail.com>
2025-07-10 22:12:28 -0400

    feat:ajustess

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM node:22-alpine
 
-# 1. Capturar la variable desde Railway (en build)
-ARG DATABASE_URL
-ENV DATABASE_URL=${DATABASE_URL}
-
 WORKDIR /app
 
 COPY package.json yarn.lock ./
@@ -11,7 +7,6 @@ RUN yarn install
 
 COPY . .
 
-# Copiar y dar permisos al script de entrada
 COPY entrypoint.sh ./entrypoint.sh
 RUN chmod +x ./entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,24 +2,5 @@
 
 set -e  # Detener si falla algo
 
-echo "Esperando la base de datos..."
-sleep 5
-
-echo "DATABASE_URL=${DATABASE_URL}"  # <-- Verificar si llega vacía
-
-if [ -z "$DATABASE_URL" ]; then
-  echo "❌ ERROR: DATABASE_URL está vacía. No se puede continuar."
-  exit 1
-fi
-
-echo "Generando cliente Prisma..."
-yarn prisma generate
-
-echo "Aplicando migraciones..."
-yarn prisma migrate deploy
-
-echo "Ejecutando seed..."
-yarn prisma db seed
-
 echo "Iniciando NestJS en modo producción..."
 yarn start:prod
